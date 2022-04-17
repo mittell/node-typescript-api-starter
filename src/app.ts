e702ff35cb1a482ebc77add04e0b13bdd1da62e7
Chris Mittell <mittell@hotmail.co.uk>
2022-04-17 14:34:35 +0900

    feat: added basic server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.status(200).send('Hello World!');
+});
+
+export default server.listen(port, () => {
+	console.log(`Server listening on port ${port}...`);
+});
