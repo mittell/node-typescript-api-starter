b80826514ebc393aabe237395013671b20372ab5
Chris Mittell <mittell@hotmail.co.uk>
2022-04-17 15:48:35 +0900

    feat: added interface for environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+import config from './config';
 import express from 'express';
 import * as http from 'http';
 import cors from 'cors';
@@ -5,7 +7,7 @@ import helmet from 'helmet';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port: number = config.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
