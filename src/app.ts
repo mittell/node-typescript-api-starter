e0893f0a80e56a55127bd1e9905b9415c596dc22
Chris Mittell <mittell@hotmail.co.uk>
2022-04-17 18:49:19 +0900

    feat: added tsoa swagger example

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
 import * as http from 'http';
 import cors from 'cors';
 import helmet from 'helmet';
+
+// @ts-ignore
+import { RegisterRoutes } from './routes';
+
+import swaggerUi from 'swagger-ui-express';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
@@ -12,8 +17,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 
+RegisterRoutes(app);
+
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.status(200).send('Hello World!');
+});
+
+app.use('/docs', swaggerUi.serve, async (req: Request, res: Response) => {
+	return res.send(
+		// @ts-ignore
+		swaggerUi.generateHTML(await import('../dist/swagger.json'))
+	);
 });
 
 export default server.listen(port, () => {
