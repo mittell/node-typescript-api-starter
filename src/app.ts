c012dfc3fbfb9ea6f322c0f720c5b00f83a55037
Chris Mittell <mittell@hotmail.co.uk>
2022-04-17 15:49:43 +0900

    fix: corrected port value for heroku

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
